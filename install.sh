38cf8a1a56b36c2b2d9f523feacb7667c67bdf48
vkolmakov <vkolmakov@hotmail.com>
2016-08-16 16:20:18 -0500

    rewrite install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,64 @@
 #!/bin/bash
-dir=~/dotfiles
-bckdir=~/dotfiles.bck
-files="zshrc emacs tmux.conf"
+create_dir() {
+    backup_dir=$1
+    mkdir -p "$backup_dir"
+}
 
-echo -n "Creating $bckdir to backup of any existing dotfiles in ~"
-mkdir -p $bckdir
-echo "Done"
+backup_file() {
+    # move src file to the dst folder
+    target=$1
+    backup_dir=$2
 
-echo -n "Changing to the $dir"
-cd $dir
-echo "Done"
+    printf "\tBacking up $target to $backup_dir\n"
+    mv "$src" "$dst"
+}
 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $bckdir"
-    mv ~/.$file $bckdir
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+create_symlink() {
+    # create symlink from src file to dst file
+    src=$1
+    dst=$2
+
+    printf "\tCreating symlink from $src to $dst\n"
+    ln -s "$src" "$dst"
+}
+
+setup_homedir() {
+    dotfiles_dir=$1
+    backup_dir=$2
+
+    home_dir_dotfiles=".zshrc .emacs .tmux.conf"
+
+    for file in $home_dir_dotfiles; do
+        backup_file "~/$file" "$backup_dir"
+        create_symlink "$dotfiles_dir/$file" "~/$file"
+    done
+}
+
+setup_karabiner() {
+    dotfiles_dir=$1
+    backup_dir=$2
+
+    karabiner_dir=~/Library/Application\ Support/Karabiner
+    karabiner_config_file=private.xml
+
+    backup_file "$karabiner_dir/$karabiner_config_file" "$backup_dir"
+    create_symlink "$dotfiles_dir/karabiner/$karabiner_config_file" "$karabiner_dir/$karabiner_config_file"
+}
+
+main() {
+    dotfiles_dir=~/dotfiles
+    backup_dir=~/dotfiles.bck
+
+    printf "* Creating $backup_dir to backup of any existing dotfiles\n"
+    create_dir "$backup_dir"
+
+    printf "* Setting up dotfiles in ~/\n"
+    setup_homedir "$dotfiles_dir" "$backup_dir"
+
+    printf "* Setting up karabiner\n"
+    setup_karabiner "$dotfiles_dir" "$backup_dir"
+
+    printf "Done!\n"
+}
+
+main
