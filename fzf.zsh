58346fc7e79b77e6a04f02143349596e98ef3894
Vladimir Kolmakov <vkolmakov256@gmail.com>
2019-08-19 23:58:40 -0500

    Clean up no longer needed files, remove dots from existing files

--- fzf.zsh
+++
@@ -1,9 +0,0 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
