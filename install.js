fe52cbd7960d3b961b5a22fcecc28a00e53e1ea8
Vladimir Kolmakov <vkolmakov256@gmail.com>
2019-08-24 21:34:17 -0500

    Add alacritty config

--- install.js
+++ install.js
@@ -56,7 +56,14 @@ function createSymlinkSync(from, to) {
 createSymlinkSync(path.resolve("zshrc"), path.join(HOME_DIR, ".zshrc"));
 createSymlinkSync(path.resolve("emacs"), path.join(HOME_DIR, ".emacs"));
 createSymlinkSync(path.resolve("vimrc"), path.join(HOME_DIR, ".vimrc"));
+
 createSymlinkSync(
   path.resolve("kitty.conf"),
   path.join(HOME_DIR, ".config", "kitty", "kitty.conf")
 );
+
+createSymlinkSync(
+  path.resolve("alacritty.yml"),
+  path.join(HOME_DIR, ".config", "alacritty", "alacritty.yml")
+);
+
